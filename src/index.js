dc786b9140e732ec693407da089fb61af5cf06fd
sarahiseffy <cloudchii@gmail.com>
2024-09-29 21:17:39 +0400

    css, html and js revision
    added better user experience format:
    -animation
    -poem element only showing when user input is submitted.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 
 function displayPoem(response) {
 
-/*let poemElement = document.querySelector("#poem1");
+/*let poemElement = document.querySelector("#poem");
 poemElement.innerHTML = `This is a sample poem.`;*/ //one solution 
 
-new Typewriter("#poem1", {
+new Typewriter("#poem", {
   strings: response.data.answer,
   autoStart: true,
   delay: 10,
@@ -16,11 +16,14 @@ event.preventDefault();
 
 let userInputElement = document.querySelector("#userInput")
 let apiKey = "47c53bba2097318c33196010f07cot74";
-let context = "You are an AI that can generate beautiful and simple four-line poem in any language the user will input and break each line with <br>."
+let context = "You are an AI that can generate beautiful poem, with 4 line and break in 3 paragraphs using <p> and in any language the user will input and break each line with <br>."
 let prompt = `Generate a poem about ${userInput.value}`
 let apiUrl =
   `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+let poemElement = document.querySelector("#poem");
+poemElement.classList.remove("hidden");
+poemElement.innerHTML = `<div class = "blink"><b>Generating poem about ${userInput.value}...</b></div>`;
 
 console.log("Generating poem..");
 axios.get(apiUrl).then(displayPoem);
