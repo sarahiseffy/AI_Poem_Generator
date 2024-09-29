4ee9e88470a162446e34c397d33216ed670510a8
sarahiseffy <cloudchii@gmail.com>
2024-09-29 20:12:12 +0400

    API setup structure and css revisions

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
 
-function generatePoem(event) {
-event.preventDefault();
+function displayPoem(response) {
 
 /*let poemElement = document.querySelector("#poem1");
 poemElement.innerHTML = `This is a sample poem.`;*/ //one solution 
 
 new Typewriter("#poem1", {
-  strings: "This is a sample poem.",
+  strings: response.data.answer,
   autoStart: true,
-  delay: 100,
+  delay: 10,
 });
-
 }
 
+function generatePoem(event) {
+event.preventDefault();
+
+let apiKey = "47c53bba2097318c33196010f07cot74";
+let context = "You are an AI that can generate beautiful and simple four-line english poems and break each line with <br>."
+let prompt = "Generate a poem."
+let apiUrl =
+  `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+
+console.log("Generating poem..");
+axios.get(apiUrl).then(displayPoem);
+
+}
 
 
 let formElement = document.querySelector("#user-input");
