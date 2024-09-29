a74337be4652ad86fff4a515ae9a0af76582c536
sarahiseffy <cloudchii@gmail.com>
2024-09-29 21:19:41 +0400

    revised context ai api

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ event.preventDefault();
 
 let userInputElement = document.querySelector("#userInput")
 let apiKey = "47c53bba2097318c33196010f07cot74";
-let context = "You are an AI that can generate beautiful poem, with 4 line and break in 3 paragraphs using <p> and in any language the user will input and break each line with <br>."
+let context = "You are an AI that can generate beautiful poem in english first, with 4 line and break in 3 paragraphs using <p> and in any language the user will input and break each line with <br>."
 let prompt = `Generate a poem about ${userInput.value}`
 let apiUrl =
   `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
